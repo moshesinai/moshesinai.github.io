270b2d88656bfc14f6b8a09b706e7387ee974a78
moshiksinai <moshiksinai1@gmail.com>
2025-06-18 17:05:02 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/moshe-sinai-realestate-investment/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
